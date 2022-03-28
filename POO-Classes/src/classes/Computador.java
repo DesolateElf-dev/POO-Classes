fa6d1b1864ed4a06acb26b1e4df2c9d0ae91151f
emaci <emaci@DESKTOP-EDU>
2022-03-28 20:10:36 -0300

    get&set computador

--- POO-Classes/src/classes/Computador.java
+++ POO-Classes/src/classes/Computador.java
@@ -2,12 +2,52 @@ package classes;
 
 public class Computador {
 	
-	String CPU;
-	String graphicsCard;
-	String motherBoard;
+	public String getCPU() {
+		return CPU;
+	}
+
+	public void setCPU(String CPU) {
+		this.CPU = CPU;
+	}
+
+	public String getGraphicsCard() {
+		return graphicsCard;
+	}
+
+	public void setGraphicsCard(String graphicsCard) {
+		this.graphicsCard = graphicsCard;
+	}
+
+	public String getMotherBoard() {
+		return motherBoard;
+	}
+
+	public void setMotherBoard(String motherBoard) {
+		this.motherBoard = motherBoard;
+	}
+
+	public int getMemory() {
+		return memory;
+	}
+
+	public void setMemory(int memory) {
+		this.memory = memory;
+	}
+
+	public int getStorageSpace() {
+		return storageSpace;
+	}
+
+	public void setStorageSpace(int storageSpace) {
+		this.storageSpace = storageSpace;
+	}
+
+	private String CPU;
+	private String graphicsCard;
+	private String motherBoard;
 	
-	int memory;
-	int storageSpace;
+	private int memory;
+	private int storageSpace;
 	
 	public static void ligar() {
 		System.out.println("Computador Ligado");
