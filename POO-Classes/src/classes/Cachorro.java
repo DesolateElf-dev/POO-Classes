588179d740fdfd45b75b39ab18e5cba6c988cf02
emaci <emaci@DESKTOP-EDU>
2022-03-28 19:53:16 -0300

    update funções cachorro

--- POO-Classes/src/classes/Cachorro.java
+++ POO-Classes/src/classes/Cachorro.java
@@ -10,14 +10,15 @@ public class Cachorro {
 	int idade;
 	
 	public static void comer() {
+		System.out.println("Comendo...");
 	}
 	
 	public static void latir() {
-		
+		System.out.println("Au au au");	
 	}
 	
 	public static void brincar() {
-		
+		System.out.println("Brincando...");		
 	}
 
 }
